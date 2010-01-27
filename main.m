d2a45963aff36f9ee42e744ce2cc3fda34c59e76
Arne Harren <ah@0xc0.de>
2010-01-27 19:57:58 +0100

    add some log statements

--- main.m
+++ main.m
@@ -2,8 +2,17 @@
 int main(int argc, char *argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
+    // enable logging for all components up to level Debug
+    lcl_configure_by_name("*", lcl_vDebug);
+    
+    // log
+    lcl_log(lcl_cMain, lcl_vInfo, @"log message %d", 1);
+    
     // do something
     printf("hello world\n");
+    
+    // log again
+    lcl_log(lcl_cMain, lcl_vInfo, @"log message %d", 2);
     
     [pool release];
     return 0;
