d4fc8ad6a15005cea572b23f8cf71340785ec50c
Arne Harren <ah@0xc0.de>
2010-01-27 20:04:20 +0100

    modify the logger configuration file to use the LogFile logger

--- lcl_config_logger.h
+++ lcl_config_logger.h
@@ -1,12 +1,21 @@
 
-#define _lcl_logger(log_component, log_level, log_format, ...) {               \
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];                \
-    NSLog(@"%s %s:%@:%d " log_format,                                          \
-          _lcl_level_header_1[log_level],                                      \
-          _lcl_component_header[log_component],                                \
-          [@__FILE__ lastPathComponent],                                       \
-          __LINE__,                                                            \
-          ## __VA_ARGS__);                                                     \
-    [pool release];                                                            \
-}
+// Use LCLLogFile as the logging backend.
+#import "LCLLogFile.h"
 
+// Tell LCLLogFile the path of the log file as an NSString.
+#define _LCLLogFile_LogFilePath                                                \
+    [NSHomeDirectory() stringByAppendingPathComponent:                         \
+        @"Library/Logs/ExampleApp/ExampleApp.log"]
+
+// Tell LCLLogFile whether it should append to an existing log file on startup,
+// instead of creating a new log file.
+#define _LCLLogFile_AppendToExistingLogFile                                    \
+    YES
+
+// Tell LCLLogFile the maximum size of a log file in bytes.
+#define _LCLLogFile_MaxLogFileSizeInBytes                                      \
+    2 * 1024 * 1024
+
+// Tell LCLLogFile whether it should mirror the log messages to stderr
+#define _LCLLogFile_MirrorMessagesToStdErr                                     \
+    NO
